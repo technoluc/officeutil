436bac90b47d6a60986a8765eeedc287a1f491fc
technoluc <technoluc@users.noreply.github.com>
2023-10-01 14:04:43 +0000

    Compile Office Utility

--- OfficeUtil.ps1
+++ OfficeUtil.ps1
@@ -84,58 +84,59 @@ function Get-OfficeScrubber {
     [string]$7zPath = "C:\Program Files\7-Zip\7z.exe"
   )
 
-  # Combineer het pad naar het archief
+  # Combine the path to the archive
   $ScrubberArchivePath = Join-Path -Path $OfficeUtilPath -ChildPath $ScrubberArchiveName
 
-  # Maak de map als deze nog niet bestaat
+  # Create the directory if it doesn't exist yet
   if (-not (Test-Path -Path $OfficeUtilPath -PathType Container)) {
     New-Item -Path $OfficeUtilPath -ItemType Directory ;
   }
 
   try {
-    # Download het archief
+    # Download the archive
     Invoke-WebRequest -Uri $ScrubberBaseUrl -OutFile $ScrubberArchivePath
 
-    # Uitpakken van het archief met het volledige pad naar 7z
-    & $7zPath x $ScrubberArchivePath -y -o"$OfficeUtilPath" > nul
+    # Extract the archive using the full path to 7z
+    & $7zPath x $ScrubberArchivePath -o"$OfficeUtilPath" -y | Out-Null
 
-    Write-Host "Het archief is succesvol gedownload en uitgepakt naar: $OfficeUtilPath"
+    Write-Host "The archive has been successfully downloaded and extracted to: $OfficeUtilPath"
   }
   catch {
-    Write-Host "Er is een fout opgetreden bij het downloaden en uitpakken van het archief: $_"
+    Write-Host "An error occurred while downloading and extracting the archive: $_"
   }
   finally {
-    # Opruimen: Verwijder het gedownloade archief
+    # Clean up: Remove the downloaded archive
     Remove-Item -Path $ScrubberArchivePath -Force
   }
 }
 function Install-7ZipIfNeeded {
-  $7ZipInstalled = Test-Path "C:\Program Files\7-Zip\7z.exe"
-
-  if (-not $7ZipInstalled) {
-      Write-Host "7-Zip is niet ge??nstalleerd. Installeren..."
-      $InstallerUrl = "https://www.7-zip.org/a/7z2301-x64.exe"
-      $InstallerPath = Join-Path -Path $env:TEMP -ChildPath "7zInstaller.exe"
-      
-      # Download het 7-Zip-installatieprogramma
-      Invoke-WebRequest -Uri $InstallerUrl -OutFile $InstallerPath -UseBasicParsing
-      
-      # Installeer 7-Zip met /S om stil te installeren
-      Start-Process -FilePath $InstallerPath -ArgumentList "/S" -Wait
-      
-      # Controleren op succesvolle installatie
-      $7ZipInstalled = Test-Path "C:\Program Files\7-Zip\7z.exe"
-      if ($7ZipInstalled) {
-          Write-Host "7-Zip is succesvol ge??nstalleerd."
-      } else {
-          Write-Host "Fout: 7-Zip-installatie is mislukt."
-      }
-
-      # Verwijder het tijdelijke installatiebestand
-      Remove-Item -Path $InstallerPath -Force
-  } else {
+    $7ZipInstalled = Test-Path "C:\Program Files\7-Zip\7z.exe"
+  
+    if (-not $7ZipInstalled) {
+        Write-Host "7-Zip is not installed. Installing..."
+        $InstallerUrl = "https://www.7-zip.org/a/7z2301-x64.exe"
+        $InstallerPath = Join-Path -Path $env:TEMP -ChildPath "7zInstaller.exe"
+        
+        # Download the 7-Zip installer
+        Invoke-WebRequest -Uri $InstallerUrl -OutFile $InstallerPath -UseBasicParsing
+        
+        # Install 7-Zip with /S for silent installation
+        Start-Process -FilePath $InstallerPath -ArgumentList "/S" -Wait
+        
+        # Check for successful installation
+        $7ZipInstalled = Test-Path "C:\Program Files\7-Zip\7z.exe"
+        if ($7ZipInstalled) {
+            Write-Host "7-Zip has been successfully installed."
+        } else {
+            Write-Host "Error: 7-Zip installation failed."
+        }
+  
+        # Remove the temporary installation file
+        Remove-Item -Path $InstallerPath -Force
+    } else {
+    }
   }
-}
+  
 Function Invoke-Logo {
     
     Clear-Host
@@ -156,12 +157,8 @@ function Process-MainMenu-Choice {
     )
 
     switch ($choice) {
-        'q' {
-            Write-Host "Afsluiten..."
-            exit
-        }
-        '0' {
-            Write-Host "Afsluiten..."
+        'q', '0' {
+            Write-Host "Exiting..."
             exit
         }
         '1' {
@@ -172,15 +169,15 @@ function Process-MainMenu-Choice {
         }
         '3' {
             Invoke-Logo
-            Write-Host " Running Massgrave.dev Microsoft Activation Scripts" -ForegroundColor Cyan 
+            Write-Host "Running Massgrave.dev Microsoft Activation Scripts" -ForegroundColor Cyan 
             Run-MAS
             Write-Host -NoNewLine "Press any key to continue... "
             $x = [System.Console]::ReadKey().KeyChar
             Show-MainMenu
         }
         default {
-            # Read-Host "Druk op Enter om door te gaan..."
-            Write-Host "Ongeldige optie. Probeer opnieuw."
+            # Read-Host "Press Enter to continue..."
+            Write-Host "Invalid option. Please try again."
             Write-Host -NoNewLine "Press any key to continue... "
             $x = [System.Console]::ReadKey().KeyChar
             Show-MainMenu
@@ -196,7 +193,7 @@ function Process-SubMenu1-Choice {
         '1' {
             Invoke-Logo
             Write-Host "Install Microsoft Office 365 Business" -ForegroundColor Green
-            # Voer hier de stappen uit voor Suboptie 1.1
+            # Perform the steps for Suboption 1.1 here
             Write-Host -NoNewLine "Press any key to continue... "
             $x = [System.Console]::ReadKey().KeyChar
             Show-SubMenu1
@@ -204,7 +201,7 @@ function Process-SubMenu1-Choice {
         '2' {
             Invoke-Logo
             Write-Host "Install Microsoft Office 2021 Pro Plus" -ForegroundColor Green
-            # Voer hier de stappen uit voor Suboptie 1.2
+            # Perform the steps for Suboption 1.2 here
             Write-Host -NoNewLine "Press any key to continue... "
             $x = [System.Console]::ReadKey().KeyChar
             Show-SubMenu1
@@ -212,23 +209,20 @@ function Process-SubMenu1-Choice {
         '3' {
             Invoke-Logo
             Write-Host "Install Microsoft Office Deployment Tool" -ForegroundColor Green
-            # Voer hier de stappen uit voor Suboptie 1.3
+            # Perform the steps for Suboption 1.3 here
             Write-Host -NoNewLine "Press any key to continue... "
             $x = [System.Console]::ReadKey().KeyChar
             Show-SubMenu1
         }
         'q' {
-            Write-Host "Afsluiten..."
+            Write-Host "Exiting..."
             exit
         }
         '0' {
             Show-MainMenu
         }
         default {
-            # Write-Host "Ongeldige optie. Probeer opnieuw."
-            # Read-Host "Druk op Enter om door te gaan..."
-            # Read-Host "Druk op Enter om door te gaan..."
-            Write-Host "Ongeldige optie. Probeer opnieuw."
+            Write-Host "Invalid option. Please try again."
             Write-Host -NoNewLine "Press any key to continue... "
             $x = [System.Console]::ReadKey().KeyChar
             Show-SubMenu1
@@ -244,7 +238,7 @@ function Process-SubMenu2-Choice {
         '1' {
             Invoke-Logo
             Write-Host "Run Office Removal Tool with SaRa" -ForegroundColor Cyan
-            # Voer hier de stappen uit voor Suboptie 1.1
+            # Perform the steps for Suboption 1.1 here
             Write-Host -NoNewLine "Press any key to continue... "
             $x = [System.Console]::ReadKey().KeyChar
             Show-SubMenu2
@@ -252,7 +246,7 @@ function Process-SubMenu2-Choice {
         '2' {
             Invoke-Logo
             Write-Host "Run Office Removal Tool with Office365 Setup" -ForegroundColor Cyan
-            # Voer hier de stappen uit voor Suboptie 1.2
+            # Perform the steps for Suboption 1.2 here
             Write-Host -NoNewLine "Press any key to continue... "
             $x = [System.Console]::ReadKey().KeyChar
             Show-SubMenu2
@@ -261,25 +255,21 @@ function Process-SubMenu2-Choice {
             Invoke-Logo
             Write-Host "Run Office Scrubber" -ForegroundColor Cyan
             Install-7ZipIfNeeded
-            # Get-OfficeScrubber
             Run-OfficeScrubber
-            # Voer hier de stappen uit voor Suboptie 1.3
+            # Perform the steps for Suboption 1.3 here
             Write-Host -NoNewLine "Press any key to continue... "
             $x = [System.Console]::ReadKey().KeyChar
             Show-SubMenu2
         }
         'q' {
-            Write-Host "Afsluiten..."
+            Write-Host "Exiting..."
             exit
         }
         '0' {
             Show-MainMenu
         }
         default {
-            # Write-Host "Ongeldige optie. Probeer opnieuw."
-            # Read-Host "Druk op Enter om door te gaan..."
-            # Read-Host "Druk op Enter om door te gaan..."
-            Write-Host "Ongeldige optie. Probeer opnieuw."
+            Write-Host "Invalid option. Please try again."
             Write-Host -NoNewLine "Press any key to continue... "
             $x = [System.Console]::ReadKey().KeyChar
             Show-SubMenu2
@@ -291,9 +281,6 @@ function Run-MAS {
   Invoke-RestMethod https://massgrave.dev/get | Invoke-Expression
 }
 function Run-OfficeScrubber {
-  Write-Host "ScrubberBaseUrl: $ScrubberBaseUrl"
-  Write-Host "OfficeUtilPath: $OfficeUtilPath"
-  Write-Host "ScrubberArchiveName: $ScrubberArchiveName"
 
   Write-Host "Select [R] Remove all Licenses option in OfficeScrubber." -ForegroundColor Yellow
   try {
@@ -308,14 +295,14 @@ function Run-OfficeScrubber {
 
 function Show-MainMenu {
   Invoke-Logo
-  Write-Host "Hoofdmenu" -ForegroundColor Green
+  Write-Host "Main Menu" -ForegroundColor Green
   Write-Host ""
   Write-Host "1. Install Microsoft Office" -ForegroundColor Green
   Write-Host "2. Uninstall Microsoft Office" -ForegroundColor Yellow
   Write-Host "3. Activate Microsoft Office / Windows" -ForegroundColor Cyan
   Write-Host "0. Exit" -ForegroundColor Red
   Write-Host ""
-  # $choice = Read-Host "Selecteer een optie (0-3)"
+  # $choice = Read-Host "Select an option (0-3)"
   Write-Host -NoNewline "Select option: "
   $choice = [System.Console]::ReadKey().KeyChar
   Write-Host ""
@@ -331,7 +318,7 @@ function Show-SubMenu1 {
   Write-Host "0. Main menu"
   Write-Host "Q. Quit"
   Write-Host ""
-  # $choice = Read-Host "Selecteer een optie (0-3)"
+  # $choice = Read-Host "Select an option (0-3)"
   Write-Host -NoNewline "Select option: "
   $choice = [System.Console]::ReadKey().KeyChar
   Write-Host ""
@@ -347,7 +334,7 @@ function Show-SubMenu2 {
   Write-Host "0. Main menu"
   Write-Host "Q. Quit"
   Write-Host ""
-  # $choice = Read-Host "Selecteer een optie (0-3)"
+  # $choice = Read-Host "Select an option (0-3)"
   Write-Host -NoNewline "Select option: "
   $choice = [System.Console]::ReadKey().KeyChar
   Write-Host ""
