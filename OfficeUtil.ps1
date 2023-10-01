69226e4bf15f83a6564af9db7b4a73862d952446
technoluc <technoluc@users.noreply.github.com>
2023-10-01 17:10:42 +0000

    Compile Office Utility

--- OfficeUtil.ps1
+++ OfficeUtil.ps1
@@ -381,19 +381,22 @@ function Stop-Script {
 # Toon het hoofdmenu
 Show-MainMenu
 
-Invoke-Logo
-Write-Host -NoNewLine "Press F to delete $OfficeUtilPath or any other key to quit: "
-$choice = [System.Console]::ReadKey().KeyChar
-Write-Host ""
-switch ($choice) {
-  'f' {
-    # Clean up: Remove the downloaded archive
-    Stop-Script
+
+if (Test-Path -Path $OfficeUtilPath -PathType Container) {
+  Invoke-Logo
+  Write-Host -NoNewLine "Press F to delete $OfficeUtilPath or any other key to quit: "
+  $choice = [System.Console]::ReadKey().KeyChar
+  Write-Host ""
+  switch ($choice) {
+    'f' {
+      # Clean up: Remove the downloaded archive
+      Stop-Script
+    }
+    'default' {
+    }
   }
-  'default' {
-    Invoke-Logo
   }
-}
+
 
 # Stop-Script
 
