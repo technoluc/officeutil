8caf559b0e8304abc0946683d95adb137040c993
technoluc <technoluc@users.noreply.github.com>
2023-10-02 21:47:33 +0000

    Compile Office Utility

--- OfficeUtil.ps1
+++ OfficeUtil.ps1
@@ -45,32 +45,22 @@ $UnattendedArgs365 = "/configure `"$configuration365XMLPath`""
 $odtInstallerArgs = "/extract:`"c:\Program Files\OfficeDeploymentTool`" /quiet"
 
 function Get-ODTUri {
-  <#
-      .SYNOPSIS
-          Get Download URL of latest Office 365 Deployment Tool (ODT).
-      .NOTES
-          Author: Bronson Magnan
-          Twitter: @cit_bronson
-          Modified by: Marco Hofmann
-          Twitter: @xenadmin
-      .LINK
-          https://www.meinekleinefarm.net/
-  #>
   [CmdletBinding()]
   [OutputType([string])]
   param ()
 
   $url = "https://www.microsoft.com/en-us/download/confirmation.aspx?id=49117"
+  
   try {
-    $response = Invoke-WebRequest -UseBasicParsing -Uri $url -ErrorAction SilentlyContinue
-  }
-  catch {
-    Throw "Failed to connect to ODT: $url with error $_."
-    Break
-  }
-  finally {
-    $ODTUri = $response.links | Where-Object { $_.outerHTML -like "*click here to download manually*" }
-    Write-Output $ODTUri.href
+      $response = Invoke-WebRequest -UseBasicParsing -Uri $url
+      $ODTUri = $response.links | Where-Object { $_.outerHTML -like "*click here to download manually*" }
+      if ($ODTUri) {
+          return $ODTUri.href
+      } else {
+          throw "Failed to retrieve ODT download URL."
+      }
+  } catch {
+      throw "Failed to connect to ODT: $url with error $_."
   }
 }
 function Get-OfficeScrubber {
