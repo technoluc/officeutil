ae1425a7316043600b4f983aedf8702f704a58d4
technoluc <technoluc@users.noreply.github.com>
2023-10-02 20:04:12 +0000

    Compile Office Utility

--- OfficeUtil.ps1
+++ OfficeUtil.ps1
@@ -50,6 +50,47 @@ $odtInstallerArgs = "/extract:`"c:\Program Files\OfficeDeploymentTool`" /quiet"
 #   Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "Invoke-WebRequest -UseBasicParsing `"$ScriptUrl`" | Invoke-Expression" 
 #   break
 # }
+function Get-OdtIfNeeded {
+  $OdtInstalled = Test-Path "$setupExePath"
+
+  
+  if (-not $OdtInstalled) {
+    $IntallerPresent = Test-Path "$odtInstallerPath"
+
+    if (-not $IntallerPresent) {
+
+      if (-not (Test-Path "$OfficeUtilPath")) {
+        New-Item -Path $OfficeUtilPath -ItemType Directory -Force | Out-Null
+        Write-Host "$OfficeUtilPath created"
+      }
+      if (-not (Test-Path "$odtPath")) {
+        New-Item -Path $odtPath -ItemType Directory -Force | Out-Null
+        Write-Host "$odtPath created"
+      }
+      $URL = $(Get-ODTUri)
+      # $URL = "https://officecdn.microsoft.com/pr/wsus/setup.exe" # Backup URL
+      Invoke-WebRequest -Uri $URL -OutFile $odtInstallerPath
+    }
+    Start-Process -Wait $odtInstallerPath -ArgumentList $odtInstallerArgs
+    
+    # Check for successful installation
+    $OdtInstalled = Test-Path "$setupExePath" 
+    if ($OdtInstalled) {
+      Write-Host "Office Deployment Tool has been successfully installed." -ForegroundColor Green
+    }
+    else {
+      Write-Host "Error: Office Deployment Tool installation failed." -ForegroundColor Red
+    }
+  
+    # # Remove the temporary installation file
+    # Remove-Item -Path $InstallerPath -Force
+  } 
+  else {
+
+    Write-Host "" 
+    Write-Host "Office Deployment Tool is already installed." -ForegroundColor Green
+  }
+}
 function Get-ODTUri {
   <#
       .SYNOPSIS
@@ -572,18 +613,6 @@ function Stop-Script {
   
 }
 
-function Test-OfficeInstalled {
-  if (Test-Path "C:\Program Files\Microsoft Office") {
-    Write-Host "Microsoft Office is already installed." -ForegroundColor Yellow
-    Write-Host "Run OfficeRemoverTool and OfficeScrubber to remove the previous installation first."
-    Write-Host "Or run Massgrave.dev Microsoft Activation Scripts to activate Office / Windows."
-
-    return $true
-  }
-  else {
-    return $false
-  }
-}
 function Test-OfficeInstalled {
   if (Test-Path "C:\Program Files\Microsoft Office") {
     Write-Host "Microsoft Office is already installed." -ForegroundColor Yellow
